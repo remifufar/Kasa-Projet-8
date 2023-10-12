5faf64bcd9c49fcc763b9abd70646ac2f1ea8a47
remi dubar <rd.dubar@gmail.com>
2023-10-13 01:44:14 +0200

    creation page about

--- src/components/Banner/BannerAbout.jsx
+++ src/components/Banner/BannerAbout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
-import AboutBanner from './Image source 2.png'
+import AboutBanner from './Imagesource2.png'
 
 const HomeBanner = () => {
     return(
